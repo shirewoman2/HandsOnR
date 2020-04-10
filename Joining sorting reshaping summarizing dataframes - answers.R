275fab37655f03d972cd036ae935eb9fa338c82e
Laura Shireman <shireman@uw.edu>
2020-04-10 11:15:39 -0700

    Added task to check that all values match. Rearranged a few answers.

--- Joining sorting reshaping summarizing dataframes - answers.R
+++ Joining sorting reshaping summarizing dataframes - answers.R
@@ -68,8 +68,9 @@ Candidates %>%
 data(Cotinine)
 head(Cotinine)
 
+
 # 11
-Cotinine <- Cotinine %>% 
+COT_wide <- Cotinine %>% 
       # We're only going to look at peak area, so let's remove peak height and
       # retention time.
       select(-Height, -RT) %>% 
@@ -79,31 +80,48 @@ Cotinine <- Cotinine %>%
              # the data you want to be listed in those new columns
       mutate(COT.PAR = COT / d3COT)
 
+# Challenge: What happens if you don't remove peak height and retention time?
+COT_wide_v2 <- Cotinine_original %>% 
+      spread(key = Analyte, value = Area)
+# Answer: Since we still have Height and RT in our data.frame, it will try to
+# match those values when it spreads the column Area. Since COT will have one
+# set of values for Area, Height, and RT and d3COT will have another set of
+# values for Area, Height, and RT, it will mean that your rows will be
+# mismatched. You'll have "NA" for d3COT whenever you have a value for COT and
+# vice versa.
+
 
 # 12
 PrePost <- data.frame(SubjectID = rep(101:200, 2),
                       StudyDay = rep(c("pre", "post"), each = 100), 
                       CD4count = c(rnorm(100, 200, 100),
-                                   rnorm(100, 450, 50)))
+                                   rnorm(100, 450, 50)),
+                      stringsAsFactors = FALSE)
 
 # Making it wide
-PrePost <- PrePost %>% 
+PrePost_wide <- PrePost %>% 
       spread(key = StudyDay, value = CD4count) %>% 
       mutate(CD4_diff = post - pre)
 
 # 13
-PrePost <- PrePost %>% 
+PrePost_long <- PrePost_wide %>% 
       select(-CD4_diff) %>% 
       gather(key = StudyDay, value = CD4count, 
              -SubjectID)
 
+ggplot(PrePost_long, aes(x = StudyDay, y = CD4count, color = SubjectID,
+                         group = SubjectID)) +
+      geom_point() + geom_line() +
+      scale_x_discrete(limits = c("pre", "post"))
 
-PrePost <- PrePost %>% 
-      mutate(StudyDay = factor(StudyDay, levels = c("pre", "post")))
 
-ggplot(PrePost, aes(x = StudyDay, y = CD4count, color = SubjectID,
-                    group = SubjectID)) +
-      geom_point() + geom_line()
+#14
+# Checking that everything matches. It does have to be listed in the same order
+# for this to work; that's why we're including the "arrange" function.
+all(arrange(PrePost, SubjectID, StudyDay) == 
+          arrange(PrePost_long, SubjectID, StudyDay))
+
+
 
 
 
